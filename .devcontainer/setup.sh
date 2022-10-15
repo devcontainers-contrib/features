c138698a54c99a4470d51ace21c0fd40d942f9f6
Daniel Braun <daniel.braun@outlook.co.il>
2022-10-15 16:13:48 +0000

    ci: devcontainer now run explainshell docker to annotate shell commands

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,4 @@
+npm install -g @devcontainers/cli
+
+# this will add hover annotations in shell script files, assuming mads-hartmann.bash-ide-vscod is installed
+docker container run --name explainshell --restart always -p 5000:5000 -d spaceinvaderone/explainshell
