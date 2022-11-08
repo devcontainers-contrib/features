04207583af89ab7167dba21040169353f395a4f2
Daniel Braun <daniel.braun@outlook.co.il>
2022-11-08 20:28:55 +0000

    fix: remove unneeded code section

--- src/cookiecutter/install.sh
+++ src/cookiecutter/install.sh
@@ -2,8 +2,6 @@
 # please review these instructions in order to contribute a pipx feature of your own: 
 UTIL_VERSION="${VERSION:-"latest"}"
 
-USERNAME=${USERNAME:-"automatic"}
-
 set -e
 
 # Clean up
@@ -27,24 +25,6 @@ updaterc() {
         fi
     fi
 }
-
-# Determine the appropriate non-root user
-
-if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
-    USERNAME=""
-    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
-        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
-            USERNAME=${CURRENT_USER}
-            break
-        fi
-    done
-    if [ "${USERNAME}" = "" ]; then
-        USERNAME=root
-    fi
-elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
-    USERNAME=root
-fi
 
 # settings these will allow us to clean leftovers later on
 export PYTHONUSERBASE=/tmp/pip-tmp
