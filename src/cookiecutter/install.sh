011ee31938d2f28abc8986a969d4b0ac12004b9c
Daniel Braun <daniel.braun@outlook.co.il>
2022-11-08 20:39:37 +0000

    fix: export variable before triggering sub shell

--- src/cookiecutter/install.sh
+++ src/cookiecutter/install.sh
@@ -38,8 +38,8 @@ if ! type pip3 > /dev/null 2>&1; then
     # additional tools we would have used that, but since it doesnt 
     # we have to disable it with INSTALL_PYTHON_TOOLS=false and install
     # pipx manually later on
-    VERSION=system \
-    INSTALL_PYTHON_TOOLS=false \
+    export VERSION="system"
+    export INSTALL_PYTHON_TOOLS="false"
     bash -c "$(curl -fsSL "https://raw.githubusercontent.com/devcontainers/features/main/src/python/install.sh")" 
 fi
 
