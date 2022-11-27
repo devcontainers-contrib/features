84c707a935fe953a5e546d60e67efea5f0477485
Daniel Braun <daniel.braun@outlook.co.il>
2022-11-27 18:06:03 +0000

    ci: dev container for own repo include shfmt and shellcheck

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,5 @@
 npm install -g @devcontainers/cli
 
+pipx install shfmt-py
 # this will add hover annotations in shell script files, assuming mads-hartmann.bash-ide-vscod is installed
 docker container run --name explainshell --restart always -p 5000:5000 -d spaceinvaderone/explainshell
