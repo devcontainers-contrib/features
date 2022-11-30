ccf11269c02b8d9ea750738a8ae46c44b3b8b1d9
Jacob Hummer <jcbhmr@outlook.com>
2022-11-29 22:19:43 -0600

    Trim the fat from the test.sh file
    I don't think we need the testing framework since it will exit
    with code non-0 whenever one of these fail.

--- test/haskell/test.sh
+++ test/haskell/test.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-
 set -e
 
-source dev-container-features-test-lib
-
-# we are simply checking existance for now.
-# full operability depends on the existance of kubectl.
-check "ghci existance" ghci --version
-check "cabal existance" cabal --version
-
-reportResults
+ghc --version
+ghci --version
+cabal --version
+stack --version
