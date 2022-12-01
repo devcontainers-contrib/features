4ea9ae810ed8c91a6d0c0d9a240423052a54b37b
Daniel Braun <daniel.braun@outlook.co.il>
2022-12-01 13:14:59 +0000

    feat: initial script library

---
+++ script-library/updaterc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+updaterc() {
+    echo "Updating /etc/bash.bashrc and /etc/zsh/zshrc..."
+    if [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
+        echo -e "$1" >> /etc/bash.bashrc
+    fi
+    if [ -f "/etc/zsh/zshrc" ] && [[ "$(cat /etc/zsh/zshrc)" != *"$1"* ]]; then
+        echo -e "$1" >> /etc/zsh/zshrc
+    fi
+}
