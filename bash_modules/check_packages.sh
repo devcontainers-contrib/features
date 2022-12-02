74bac8512d449718630b9c3cfa18d5b5b210ab28
Jacob Hummer <jcbhmr@outlook.com>
2022-12-02 01:49:41 +0000

    Add the first bash_modules item

---
+++ bash_modules/check_packages.sh
@@ -0,0 +1,10 @@
+#shellcheck shell=bash
+check_packages() {
+	if ! dpkg -s "$@" >/dev/null 2>&1; then
+		if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+			echo "Running apt-get update..."
+			apt-get update -y
+		fi
+		apt-get -y install --no-install-recommends "$@"
+	fi
+}
