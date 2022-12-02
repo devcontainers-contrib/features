9c8f958ffcf6d3eb925215395f33fab6ccf15dbb
Jacob Hummer <jcbhmr@outlook.com>
2022-12-02 03:06:14 +0000

    Change back to update_rc & add docs about /etc/* stuff

--- bash_modules/update_rc.sh
+++ bash_modules/update_rc.sh
@@ -1,5 +1,5 @@
 #shellcheck shell=bash
-add_to_rc() {
+update_rc() {
     # The *"..."* asterisk syntax is Bash syntax for "matches".
     # https://superuser.com/a/1582420
     #
@@ -8,12 +8,18 @@ add_to_rc() {
     #
     # The -e argument to echo makes it interpret \r\n\t escape sequences.
     # https://cheat.sh/echo
+    #
+    # Why /etc/* stuff instead of ~/.bashrc? I don't really know. All I know is
+    # that the official devcontainer installation of Node.js does it this way,
+    # so I assume it's the right way to do it.
+    # https://github.com/devcontainers/features/blob/main/src/node/install.sh#L54
 
-    echo "Updating /etc/bash.bashrc..."
+    echo "Adding '$1' to /etc/bash.bashrc ..."
     if [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
         echo -e "$1" >> /etc/bash.bashrc
     fi
-    echo "Updating /etc/zsh/zshrc"
+    
+    echo "Adding '$1' to /etc/zsh/zshrc ..."
     if [[ -f /etc/zsh/zshrc ]] && [[ "$(cat /etc/zsh/zshrc)" != *"$1"* ]]; then
         echo -e "$1" >> /etc/zsh/zshrc
     fi
