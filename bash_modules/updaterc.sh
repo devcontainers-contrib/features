33a10dfba6b75ce4c58e28dffbc15060f0baffcb
Jacob Hummer <jcbhmr@outlook.com>
2022-12-02 02:25:55 +0000

    Add the updaterc function to bash_modules

---
+++ bash_modules/updaterc.sh
@@ -0,0 +1,10 @@
+#shellcheck shell=bash
+updaterc() {
+    echo "Updating /etc/bash.bashrc and /etc/zsh/zshrc..."
+    if [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
+        echo -e "$1" >> /etc/bash.bashrc
+    fi
+    if [ -f "/etc/zsh/zshrc" ] && [[ "$(cat /etc/zsh/zshrc)" != *"$1"* ]]; then
+        echo -e "$1" >> /etc/zsh/zshrc
+    fi
+}
