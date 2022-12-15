229662c4969c729d42d6ad833a2b8469a7c9b69b
Daniel Braun <braundaniel@live.com>
2022-12-15 16:58:45 +0000

    added progress

--- tests/test_all_features.py
+++ tests/test_all_features.py
@@ -1,6 +1,7 @@
 import ray
 import os
 import pytest
+from tqdm import tqdm
 
 
 @pytest.fixture(scope="session")
@@ -10,6 +11,17 @@ def base_dir(pytestconfig):
 @pytest.fixture(scope="session")
 def image(pytestconfig):
     return pytestconfig.getoption("image")
+
+def ray_get_iterator(obj_ids, verbose: bool):
+    while obj_ids:
+        done, obj_ids = ray.wait(obj_ids)
+        ret= ray.get(done[0])
+        if verbose:
+            print(ret.stdout)
+            print(ret.stderr)
+        yield ret
+
+
 
 
 def test_assert_good_exitcode(shell, base_dir: str, image: str):
@@ -25,10 +37,9 @@ def test_assert_good_exitcode(shell, base_dir: str, image: str):
 
     feature_names = os.listdir(os.path.join(base_dir ,"src"))
     futures = [remote_ray_task.remote(shell, get_devcontainer_shell_args(base_dir,feature_name, image)) for feature_name in feature_names]
-    rets = ray.get(futures)
-    for ret in rets:
-        print(ret.stdout)
-        print(ret.stderr)
 
+    rets = tqdm(ray_get_iterator(futures, verbose=True), total=len(futures))
+
+    # for ret in rets:
     for ret in rets:
         assert ret.returncode == 0
