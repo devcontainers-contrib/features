d7cbb14c624198cd401d07208bc302e35261f5b1
Daniel Braun <braundaniel@live.com>
2022-12-16 19:15:14 +0000

    prune without -a

--- tests/test_all_features.py
+++ tests/test_all_features.py
@@ -39,9 +39,9 @@ def test_assert_good_exitcode(shell, base_dir: str, image: str, feature_ids: lis
         ret = shell.run(args , shell=True)
         return ret
 
-    def get_devcontainer_shell_args(base_dir: str, feature_id: str, image: str) -> list[str]:
-        return f"devcontainer features test -p {base_dir} -f {feature_id} -i {image} && docker system prune -a -f && docker buildx prune -a -f --keep-storage=1gb --verbose"
-        
+    def get_devcontainer_shell_args(base_dir: str, feature_id: str, image: str) -> str:
+        return f"devcontainer features test -p {base_dir} -f {feature_id} -i {image} && docker system prune -f && docker buildx prune -a -f --keep-storage=1gb --verbose"
+
     futures = [remote_ray_task.remote(shell, get_devcontainer_shell_args(base_dir,feature_id, image)) for feature_id in feature_ids]
 
     rets = tqdm(ray_get_iterator(futures, verbose=True), total=len(futures))
