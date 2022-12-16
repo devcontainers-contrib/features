e40281331d86880175a8220750d192f24e6f0f5d
Daniel Braun <braundaniel@live.com>
2022-12-16 20:20:35 +0000

    better chunks and pruning

--- tests/chunk_json_stdin.py
+++
@@ -1,4 +0,0 @@
-import sys
-import json
-_input = json.loads(sys.stdin.read())
-print(json.dumps(_input[int(sys.argv[2])::int(sys.argv[1])]), flush=True)
--- tests/test_all_features.py
+++ tests/test_all_features.py
@@ -1,10 +1,12 @@
 import ray
-import os
-from typing import Optional
 import pytest
 import json
-
+import time
+from more_itertools import chunked
 from tqdm import tqdm
+
+CHUNK_SIZE= 10
+
 
 @pytest.fixture(scope="session")
 def base_dir(pytestconfig):
@@ -32,7 +34,9 @@ def ray_get_iterator(obj_ids, verbose: bool):
 
 def test_assert_good_exitcode(shell, base_dir: str, image: str, feature_ids: list[str]):
 
+    feature_ids_chunks = list(chunked(feature_ids, CHUNK_SIZE))
     ray.init(address='auto', log_to_driver=True, logging_level=0)
+
 
     @ray.remote
     def remote_ray_task(shell, args):
@@ -42,11 +46,20 @@ def test_assert_good_exitcode(shell, base_dir: str, image: str, feature_ids: lis
     def get_devcontainer_shell_args(base_dir: str, feature_id: str, image: str) -> str:
         return f"devcontainer features test -p {base_dir} -f {feature_id} -i {image}"
 
-    futures = [remote_ray_task.remote(shell, get_devcontainer_shell_args(base_dir,feature_id, image)) for feature_id in feature_ids]
+    bad_rets = []
+    for feature_ids_chunk in feature_ids_chunks:
+            
+        futures = [remote_ray_task.remote(shell, get_devcontainer_shell_args(base_dir,feature_id, image)) for feature_id in feature_ids_chunk]
 
-    rets = tqdm(ray_get_iterator(futures, verbose=True), total=len(futures))
+        rets = tqdm(ray_get_iterator(futures, verbose=True), total=len(futures))
 
-    # for ret in rets:
-    bad_rets = [ret for ret in rets if ret.returncode != 0]
+        # for ret in rets:
+        bad_rets.extend([ret for ret in rets if ret.returncode != 0])
+    
+        time.sleep(1)
+
+        print(shell.run("docker system prune -af", shell=True).stdout)
+        print(shell.run("docker image prune -af", shell=True).stdout)
+        print(shell.run("docker buildx prune -af", shell=True).stdout)
 
     assert len(bad_rets) == 0, f"{len(bad_rets)} features failed:\n {bad_rets}"
