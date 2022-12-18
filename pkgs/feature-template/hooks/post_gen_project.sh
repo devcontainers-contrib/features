a52a0b046fb685a80184883e72f8bc83481bfb54
Jacob Hummer <jcbhmr@outlook.com>
2022-12-18 03:10:47 +0000

    Remove cookiecutter stuff

--- pkgs/feature-template/hooks/post_gen_project.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-shfmt -l -w ./src/{{ cookiecutter.feature_id }}/install.sh
-
-shfmt -l -w ./test/{{ cookiecutter.feature_id }}/test.sh
--- pkgs/feature-template/{{cookiecutter.base_dir}}/test/{{cookiecutter.feature_id}}/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-source dev-container-features-test-lib
-
-check "{{cookiecutter.test_command}}" {{cookiecutter.test_command}}
-
-reportResults
