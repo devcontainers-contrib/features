a33aa0957dfa61e28bda1578db02a2a3ec276b06
Jacob Hummer <jcbhmr@outlook.com>
2023-01-26 14:30:46 -0600

    Add $1 arg to test.sh

--- tools/test.sh
+++ tools/test.sh
@@ -3,14 +3,19 @@
 echo "Testing global scenarios"
 devcontainer features test --global-scenarios-only .
 
-for id in test/*; do
-  if [[ -f $id ]]; then
-    continue
-  fi
+if [[ -z $1 ]]; do
+  for id in test/*; do
+    if [[ -f $id ]]; then
+      continue
+    fi
 
-  id="$(basename "$id")"
+    id="$(basename "$id")"
 
-  echo "Testing $id..."
-  devcontainer features test -f "$id" .
-  echo "Done! Passed! $id"
-done
+    echo "Testing $id..."
+    devcontainer features test -f "$id" .
+    echo "Done! Passed! $id"
+  done
+else
+  echo "Testing ONLY $1"
+  devcontainer features test -f "$1" .
+fi
