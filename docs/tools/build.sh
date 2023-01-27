2cb5fd15ac4d12a06d5b8f7e1fca123bab0013f8
Jacob Hummer <jcbhmr@outlook.com>
2023-01-27 23:03:16 +0000

    Use set -e instead of /bin/bash -e to get shellcheck to detect them

--- docs/tools/build.sh
+++ docs/tools/build.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
 
 anyssg() {
   local ua='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
--- docs/tools/dev.sh
+++ docs/tools/dev.sh
@@ -1,3 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
 
 php -S 0.0.0.0:8000 -c . -t pages router.php
