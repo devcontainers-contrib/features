6e83b5a0964a0a130f74f9954b9238133cd677d5
Jacob Hummer <jcbhmr@outlook.com>
2023-01-26 23:20:54 -0600

    Tighten devcontainer config

--- .devcontainer/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-npm install -g @devcontainers/cli
-
-pipx install shfmt-py
-
-pip install -r requirements-tests.txt
-# this will add hover annotations in shell script files, assuming mads-hartmann.bash-ide-vscod is installed
-docker container run --name explainshell --restart always -p 5000:5000 -d spaceinvaderone/explainshell
