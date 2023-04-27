3cb585a971d09bccd64c290a3922e4703ccfad11
Daniel Braun <braundaniel@live.com>
2023-04-27 15:41:55 +0300

    fix: bump apt-get-packages dependants to 1.0.4 (#390)

--- src/micro/install.sh
+++ src/micro/install.sh
@@ -9,13 +9,13 @@ source ./library_scripts.sh
 # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
 # and if missing - will download a temporary copy that automatically get deleted at the end 
 # of the script
-ensure_nanolayer nanolayer_location "v0.4.27"
+ensure_nanolayer nanolayer_location "v0.4.28"
 
 
 $nanolayer_location \
     install \
     devcontainer-feature \
-    "ghcr.io/devcontainers-contrib/features/apt-get-packages:1.0.3" \
+    "ghcr.io/devcontainers-contrib/features/apt-get-packages:1.0.4" \
     --option packages='micro'
 
 
