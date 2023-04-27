0f25948fe63cd024747185eaebad9c2a7063a8f6
Daniel Braun <braundaniel@live.com>
2023-04-27 18:42:55 +0300

    Update setup.sh (#393)
    Signed-off-by: Daniel Braun <braundaniel@live.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,6 +4,5 @@ npm install -g @devcontainers/cli
 
 pipx install shfmt-py
 
-pip install -r requirements-tests.txt
 # this will add hover annotations in shell script files, assuming mads-hartmann.bash-ide-vscod is installed
 docker container run --name explainshell --restart always -p 5000:5000 -d spaceinvaderone/explainshell
