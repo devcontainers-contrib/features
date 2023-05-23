ce2a27ec549b66fafd3cbdc3586bae70574ad9fb
Daniel Braun <braundaniel@live.com>
2023-05-23 17:35:24 +0300

    Danielbraun89/issue416 (#442)
    * fix: clean up asdf-package
    
    * Add feature for rabbitMQ
    Fixes #416

---
+++ src/rabbitmq-asdf/README.md
@@ -0,0 +1,21 @@
+
+# Rabbitmq (via asdf) (rabbitmq-asdf)
+
+RabbitMQ is a feature rich, multi-protocol messaging and streaming broker.
+
+## Example DevContainer Usage
+
+```json
+"features": {
+    "ghcr.io/devcontainers-contrib/features/rabbitmq-asdf:1": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| version | Select the version to install. | string | latest |
+| erlangVersion | Select the version to install. | string | latest |
+
+
---
+++ src/rabbitmq-asdf/install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -i
+
+set -e
+
+source ./library_scripts.sh
+
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-contrib/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
+# and if missing - will download a temporary copy that automatically get deleted at the end 
+# of the script
+ensure_nanolayer nanolayer_location "v0.4.39"
+
+
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/apt-get-packages:1.0.4" \
+    --option packages='build-essential,autoconf,m4,libncurses5-dev,libwxgtk3.0-gtk3-dev,libwxgtk-webview3.0-gtk3-dev,libgl1-mesa-dev,libglu1-mesa-dev,libpng-dev,libssh-dev,unixodbc-dev,xsltproc,fop,libxml2-utils,libncurses-dev,openjdk-11-jdk'
+    
+
+
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/asdf-package:1.0.7" \
+    --option plugin='erlang' --option version="$ERLANGVERSION"
+    
+
+
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/apt-get-packages:1.0.4" \
+    --option packages='xz-utils'
+    
+
+
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/asdf-package:1.0.7" \
+    --option plugin='rabbitmq' --option version="$VERSION" --option latestVersionPattern='v'
+    
+
+
+echo 'Done!'
+
