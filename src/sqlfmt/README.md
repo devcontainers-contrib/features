84cb422b2e9cb128847c3b0c682d5473c44c9fa2
Daniel Bartley <dbrtly00@gmail.com>
2023-06-25 21:34:19 +1000

    Add sqlfmt feature (#466)
    * add sqlfmt feature
    
    * tests: add tests for sqlfmt
    
    * Update README.md
    
    (feature version is independent of the package version - package version should be an input to the feature)
    
    
    Signed-off-by: Daniel Braun <daniel.braun@outlook.co.il>
    
    * Update devcontainer-feature.json
    
    (feature version is independent of the package version - package version should be an input to the feature)
    
    Signed-off-by: Daniel Braun <daniel.braun@outlook.co.il>
    
    * Update install.sh
    
    updated pipx-pacakge and nanolayer versions
    
    Signed-off-by: Daniel Braun <daniel.braun@outlook.co.il>
    
    ---------
    
    Signed-off-by: Daniel Braun <daniel.braun@outlook.co.il>
    Co-authored-by: Daniel Bartley <dbrtly00@.gmail.com>
    Co-authored-by: Daniel Braun <daniel.braun@outlook.co.il>

---
+++ src/sqlfmt/README.md
@@ -0,0 +1,20 @@
+
+# sqlfluff (via pipx) (sqlfluff)
+
+sqlfmt formats your dbt SQL files so you don't have to.
+
+## Example DevContainer Usage
+
+```json
+"features": {
+    "ghcr.io/devcontainers-contrib/features/shandy-sqlfmt:1.0.0": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| version | Select the version to install. | string | latest |
+
+
---
+++ src/sqlfmt/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -i
+
+set -e
+
+source ./library_scripts.sh
+
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-contrib/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
+# and if missing - will download a temporary copy that automatically get deleted at the end 
+# of the script
+ensure_nanolayer nanolayer_location "v0.4.45"
+
+
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/pipx-package:1.1.6" \
+    --option package='shandy-sqlfmt[jinjafmt]' --option version="$VERSION"
+
+
+echo 'Done!'
+
