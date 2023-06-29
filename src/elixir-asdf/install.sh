cb1fef351397684092e21d2d5c26ca33589812a3
Daniel Braun <braundaniel@live.com>
2023-06-29 15:53:19 +0300

    Bug: Erlang feature works on only some base images, fails on others (#483)

--- src/elixir-asdf/install.sh
+++ src/elixir-asdf/install.sh
@@ -16,14 +16,14 @@ $nanolayer_location \
     install \
     devcontainer-feature \
     "ghcr.io/devcontainers-contrib/features/apt-get-packages:1.0.4" \
-    --option packages='build-essential,autoconf,m4,libncurses5-dev,libwxgtk3.0-gtk3-dev,libwxgtk-webview3.0-gtk3-dev,libgl1-mesa-dev,libglu1-mesa-dev,libpng-dev,libssh-dev,unixodbc-dev,xsltproc,fop,libxml2-utils,libncurses-dev,openjdk-11-jdk'
+    --option packages='build-essential,autoconf,m4,libncurses5-dev,libwxgtk3.0-gtk3-dev,libwxgtk-webview3.0-gtk3-dev,libgl1-mesa-dev,libglu1-mesa-dev,libpng-dev,libssh-dev,unixodbc-dev,xsltproc,fop,libxml2-utils,libncurses-dev,openjdk-11-jdk,procps'
     
 
 
 $nanolayer_location \
     install \
     devcontainer-feature \
-    "ghcr.io/devcontainers-contrib/features/asdf-package:1.0.5" \
+    "ghcr.io/devcontainers-contrib/features/asdf-package:1.0.8" \
     --option plugin='erlang' --option version="$ERLANGVERSION"
     
 
@@ -31,7 +31,7 @@ $nanolayer_location \
 $nanolayer_location \
     install \
     devcontainer-feature \
-    "ghcr.io/devcontainers-contrib/features/asdf-package:1.0.5" \
+    "ghcr.io/devcontainers-contrib/features/asdf-package:1.0.8" \
     --option plugin='elixir' --option version="$ELIXIRVERSION"
     
 
