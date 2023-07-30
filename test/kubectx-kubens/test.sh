68997b2905d8ae285921094dbf6e55a0826d3012
Nate Scherer <376408+natescherer@users.noreply.github.com>
2023-07-30 01:03:31 -0400

    Fix typo

--- test/kubectx-kubens/test.sh
+++ test/kubectx-kubens/test.sh
@@ -4,7 +4,7 @@ set -e
 
 source dev-container-features-test-lib
 
-# we are simply version for now.
+# we are simply checking version for now.
 # full operability depends on the existance of kubectl.
 check "kubectx version" kubectx --version
 check "kubens version" kubens --version
