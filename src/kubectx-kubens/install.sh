4d15b678f288913209e74bf43789b63fccc25fa4
Nate Scherer <376408+natescherer@users.noreply.github.com>
2023-08-20 05:56:32 -0400

    Update kubectx-kubens to install Go versions (fixes #459) (#499)
    * Update kubectx-kubens to install Go versions (fixes #459)
    
    * Fix typo

--- src/kubectx-kubens/install.sh
+++ src/kubectx-kubens/install.sh
@@ -1,72 +1,33 @@
 #!/usr/bin/env bash
 
-KUBCTX_KUBENS_VERSION="${VERSION:-"latest"}"
+KUBECTX_KUBENS_VERSION="${VERSION:-"latest"}"
 
 set -e
 
-# Clean up
-rm -rf /var/lib/apt/lists/*
+source ./library_scripts.sh
+
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-contrib/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
+# and if missing - will download a temporary copy that automatically get deleted at the end 
+# of the script
+ensure_nanolayer nanolayer_location "v0.4.45"
 
 if [ "$(id -u)" -ne 0 ]; then
 	echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
 	exit 1
 fi
 
-# Checks if packages are installed and installs them if not
-check_packages() {
-	if ! dpkg -s "$@" >/dev/null 2>&1; then
-		if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-			echo "Running apt-get update..."
-			apt-get update -y
-		fi
-		apt-get -y install --no-install-recommends "$@"
-	fi
-}
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/gh-release:1" \
+    --option repo='ahmetb/kubectx' --option binaryNames='kubectx' --option version="$KUBECTX_KUBENS_VERSION" --option assetRegex="kubectx.*"
 
-# Figure out correct version of a three part version number is not passed
-find_version_from_git_tags() {
-	local variable_name=$1
-	local requested_version=${!variable_name}
-	if [ "${requested_version}" = "none" ]; then return; fi
-	local repository=$2
-	local prefix=${3:-"tags/v"}
-	local separator=${4:-"."}
-	local last_part_optional=${5:-"false"}
-	if [ "$(echo "${requested_version}" | grep -o "." | wc -l)" != "2" ]; then
-		local escaped_separator=${separator//./\\.}
-		local last_part
-		if [ "${last_part_optional}" = "true" ]; then
-			last_part="(${escaped_separator}[0-9]+)?"
-		else
-			last_part="${escaped_separator}[0-9]+"
-		fi
-		local regex="${prefix}\\K[0-9]+${escaped_separator}[0-9]+${last_part}$"
-		local version_list="$(git ls-remote --tags ${repository} | grep -oP "${regex}" | tr -d ' ' | tr "${separator}" "." | sort -rV)"
-		if [ "${requested_version}" = "latest" ] || [ "${requested_version}" = "current" ] || [ "${requested_version}" = "lts" ]; then
-			declare -g ${variable_name}="$(echo "${version_list}" | head -n 1)"
-		else
-			set +e
-			declare -g ${variable_name}="$(echo "${version_list}" | grep -E -m 1 "^${requested_version//./\\.}([\\.\\s]|$)")"
-			set -e
-		fi
-	fi
-	if [ -z "${!variable_name}" ] || ! echo "${version_list}" | grep "^${!variable_name//./\\.}$" >/dev/null 2>&1; then
-		echo -e "Invalid ${variable_name} value: ${requested_version}\nValid values:\n${version_list}" >&2
-		exit 1
-	fi
-	echo "${variable_name}=${!variable_name}"
-}
-
-find_version_from_git_tags KUBCTX_KUBENS_VERSION 'https://github.com/ahmetb/kubectx'
-
-check_packages ca-certificates curl
-
-curl -L -o /usr/local/bin/kubectx https://raw.githubusercontent.com/ahmetb/kubectx/v${KUBCTX_KUBENS_VERSION}/kubectx &&
-	curl -L -o /usr/local/bin/kubens https://raw.githubusercontent.com/ahmetb/kubectx/v${KUBCTX_KUBENS_VERSION}/kubens &&
-	chmod +x /usr/local/bin/kubectx &&
-	chmod +x /usr/local/bin/kubens
-
-# Clean up
-rm -rf /var/lib/apt/lists/*
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/gh-release:1" \
+    --option repo='ahmetb/kubectx' --option binaryNames='kubens' --option version="$KUBECTX_KUBENS_VERSION" --option assetRegex="kubens.*"
 
 echo "Done!"
--- test/kubectx-kubens/test.sh
+++ test/kubectx-kubens/test.sh
@@ -4,9 +4,9 @@ set -e
 
 source dev-container-features-test-lib
 
-# we are simply checking existance for now.
+# we are simply checking version for now.
 # full operability depends on the existance of kubectl.
-check "kubectx existance" which kubectx
-check "kubens existance" which kubens
+check "kubectx version" kubectx --version
+check "kubens version" kubens --version
 
 reportResults
