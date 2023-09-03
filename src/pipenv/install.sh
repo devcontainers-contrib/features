756258dea0236eaa85b163dd5d2c65e73afa2aa6
Daniel Braun <braundaniel@live.com>
2023-09-03 17:04:38 +0300

    [Bug]: Codespaces creation fails when pre-commit feature is enabled (#515)
    * [Bug]: Codespaces creation fails when pre-commit feature is enabled
    
    * fix: shell lint
    
    * ci: disable missing shebang

--- src/pipenv/install.sh
+++ src/pipenv/install.sh
@@ -1,21 +1,20 @@
-#!/bin/bash -i
 
 set -e
 
-source ./library_scripts.sh
+. ./library_scripts.sh
 
 # nanolayer is a cli utility which keeps container layers as small as possible
 # source code: https://github.com/devcontainers-contrib/nanolayer
 # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
 # and if missing - will download a temporary copy that automatically get deleted at the end 
 # of the script
-ensure_nanolayer nanolayer_location "v0.4.45"
+ensure_nanolayer nanolayer_location "v0.5.0"
 
 
 $nanolayer_location \
     install \
     devcontainer-feature \
-    "ghcr.io/devcontainers-contrib/features/pipx-package:1.1.6" \
+    "ghcr.io/devcontainers-contrib/features/pipx-package:1.1.7" \
     --option package='pipenv' --option version="$VERSION"
     
 
