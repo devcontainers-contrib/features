0a0a71352c5f0a02c102c533e1982d6ac253cfa9
Daniel Braun <braundaniel@live.com>
2023-09-05 17:49:30 +0300

    New nanolayer version for gh-release feature (#516)
    * [Bug]: Can't download multiple assets from GH Releases
    
    * fix: make gh-release non bash dependent

--- src/gh-release/install.sh
+++ src/gh-release/install.sh
@@ -1,22 +1,21 @@
-#!/bin/bash -i
 
 set -e
 
-source ./library_scripts.sh
+. ./library_scripts.sh
 
 # nanolayer is a cli utility which keeps container layers as small as possible
 # source code: https://github.com/devcontainers-contrib/nanolayer
 # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
 # and if missing - will download a temporary copy that automatically get deleted at the end 
 # of the script
-ensure_nanolayer nanolayer_location "v0.4.45"
+ensure_nanolayer nanolayer_location "v0.5.2"
 
 
 
 
-release_tag_regex=$(if ! [[ -z $RELEASETAGREGEX ]]; then echo --release-tag-regex "${RELEASETAGREGEX}" ; fi)
-asset_regex_cmd=$(if ! [[ -z $ASSETREGEX ]]; then echo --asset-regex "${ASSETREGEX}" ; fi)
-lib_name_cmd=$(if ! [[ -z $LIBNAME ]]; then echo --lib-name "${LIBNAME}" ; fi)
+release_tag_regex=$(if ! [ -z $RELEASETAGREGEX ]; then echo --release-tag-regex "${RELEASETAGREGEX}" ; fi)
+asset_regex_cmd=$(if ! [ -z $ASSETREGEX ]; then echo --asset-regex "${ASSETREGEX}" ; fi)
+lib_name_cmd=$(if ! [ -z $LIBNAME ]; then echo --lib-name "${LIBNAME}" ; fi)
 $nanolayer_location \
    install gh-release \
    "$REPO" \
