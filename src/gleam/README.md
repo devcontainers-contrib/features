4b9c934b2e8a0ccac1fbd1ba7b1e32675983a1fb
Daniel Braun <braundaniel@live.com>
2023-09-30 14:52:00 +0000

    [Feature Request]: Gleam CLI Fixes #535

---
+++ src/gleam/README.md
@@ -0,0 +1,20 @@
+
+# Gleam (via Github Releases) (gleam)
+
+Gleam is a friendly language for building type-safe, scalable systems. (Gleam compiles to Erlang code, so Erlang needs to be installed to run Gleam code.)
+
+## Example DevContainer Usage
+
+```json
+"features": {
+    "ghcr.io/devcontainers-contrib/features/gleam:1": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| version | Select the version to install. | string | latest |
+
+
---
+++ src/gleam/install.sh
@@ -0,0 +1,23 @@
+
+set -e
+
+. ./library_scripts.sh
+
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-contrib/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
+# and if missing - will download a temporary copy that automatically get deleted at the end 
+# of the script
+ensure_nanolayer nanolayer_location "v0.5.5"
+
+
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.24" \
+    --option repo='gleam-lang/gleam' --option binaryNames='gleam' --option version="$VERSION"
+    
+
+
+echo 'Done!'
+
