6c1b531d52ec6c3c95cc0cf30c308d5d436eeef9
koralowiec <36413794+koralowiec@users.noreply.github.com>
2024-09-13 08:37:25 +0000

    fix(ufmt): bump pipx-package version

--- src/ufmt/install.sh
+++ src/ufmt/install.sh
@@ -6,19 +6,15 @@ source ./library_scripts.sh
 
 # nanolayer is a cli utility which keeps container layers as small as possible
 # source code: https://github.com/devcontainers-contrib/nanolayer
-# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
-# and if missing - will download a temporary copy that automatically get deleted at the end 
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations,
+# and if missing - will download a temporary copy that automatically get deleted at the end
 # of the script
 ensure_nanolayer nanolayer_location "v0.4.45"
-
 
 $nanolayer_location \
     install \
     devcontainer-feature \
-    "ghcr.io/devcontainers-extra/features/pipx-package:1.1.6" \
+    "ghcr.io/devcontainers-extra/features/pipx-package:1.1.8" \
     --option package='ufmt' --option version="$VERSION"
-    
-
 
 echo 'Done!'
-
