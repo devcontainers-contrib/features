cadfcf05e2bd958330f8f1d1f645bafc2a2772a3
koralowiec <36413794+koralowiec@users.noreply.github.com>
2024-09-13 10:18:50 +0000

    fix(azure-apiops): update regex for assets

--- src/azure-apiops/install.sh
+++ src/azure-apiops/install.sh
@@ -1,31 +1,24 @@
-
 set -e
 
 . ./library_scripts.sh
 
 # nanolayer is a cli utility which keeps container layers as small as possible
 # source code: https://github.com/devcontainers-contrib/nanolayer
-# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
-# and if missing - will download a temporary copy that automatically get deleted at the end 
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations,
+# and if missing - will download a temporary copy that automatically get deleted at the end
 # of the script
 ensure_nanolayer nanolayer_location "v0.5.4"
 
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.23" \
+    --option repo='Azure/apiops' --option binaryNames='extractor' --option assetRegex='^extractor-linux-x64.zip' --option version="$VERSION"
 
 $nanolayer_location \
     install \
     devcontainer-feature \
     "ghcr.io/devcontainers-contrib/features/gh-release:1.0.23" \
-    --option repo='Azure/apiops' --option binaryNames='extractor' --option assetRegex='^extractor.linux-x64.exe' --option version="$VERSION"
-    
-
-
-$nanolayer_location \
-    install \
-    devcontainer-feature \
-    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.23" \
-    --option repo='Azure/apiops' --option binaryNames='publisher' --option assetRegex='^publisher.linux-x64.exe' --option version="$VERSION"
-    
-
+    --option repo='Azure/apiops' --option binaryNames='publisher' --option assetRegex='^publisher-linux-x64.zip' --option version="$VERSION"
 
 echo 'Done!'
-
