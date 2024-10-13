ab15c304412eed7344f92c386e7a507f11f2e522
koralowiec <36413794+koralowiec@users.noreply.github.com>
2024-10-13 08:27:28 +0000

    feat: add aws-sso-cli feature

---
+++ src/aws-sso-cli/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+source ./library_scripts.sh
+
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-contrib/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations,
+# and if missing - will download a temporary copy that automatically get deleted at the end
+# of the script
+ensure_nanolayer nanolayer_location "v0.5.6"
+
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-extra/features/gh-release:1" \
+    --option repo='synfinatic/aws-sso-cli' --option binaryNames='aws-sso' \
+    --option version="$VERSION" --option releaseTagRegex='^(?!.*beta).*$'
+
+echo 'Done!'
