ee5094b5e788a0b16c58762f01d0d57aec394f16
klezm <20573323+klezm@users.noreply.github.com>
2024-11-04 12:52:43 +0000

    Use smaller package for testing

--- test/haskell/test_libraries.sh
+++ test/haskell/test_libraries.sh
@@ -4,6 +4,6 @@ set -e
 
 source dev-container-features-test-lib
 
-check "cabal install --lib" ghc -e "import Test.Hspec; import Test.QuickCheck"
+check "cabal install --lib" ghc -e "import Control.Concurrent.STM"
 
 reportResults
