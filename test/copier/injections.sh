6e01abdc655b6f0722065ec8cd3771757cd86400
Michał Bachowski <michalbachowski@gmail.com>
2024-12-13 12:29:01 +0000

    Fix tests

--- test/copier/injections.sh
+++ test/copier/injections.sh
@@ -5,6 +5,6 @@ set -e
 source dev-container-features-test-lib
 
 check "copier --version" copier --version
-check "copier has jinja2-strcase extension available" bash -c 'find $(dirname $(dirname $(which jinja2))) -name "jinja2_strcase" | grep jinja2_strcase'
+check "copier has jinja2-strcase extension available" bash -c 'find $(dirname $(dirname $(which copier))) -name "jinja2_strcase" | grep jinja2_strcase'
 
 reportResults
