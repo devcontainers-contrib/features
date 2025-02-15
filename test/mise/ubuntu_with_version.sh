f526ceaa3a5193993c33a4c7d9a5dd44469474b4
Pierre-Emmanuel Mercier <acesyde@gmail.com>
2025-02-15 11:34:08 +0100

    fix: code review

--- test/mise/ubuntu_with_version.sh
+++ test/mise/ubuntu_with_version.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "2025.2.0" mise -v
+check "mise version is equal to 2025.2.0" sh -c "mise -v | grep '2025.2.0'"
 
 # Report result
 reportResults
