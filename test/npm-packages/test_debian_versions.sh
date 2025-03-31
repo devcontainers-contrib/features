f28a554285c77f5a0929081e633a6328a1ebe937
George Cover <5285122+TheOptimist@users.noreply.github.com>
2025-03-31 11:53:20 -0400

    fix(npm-packages): fix test for scoped npm package installation

--- test/npm-packages/test_debian_versions.sh
+++ test/npm-packages/test_debian_versions.sh
@@ -7,7 +7,7 @@ source dev-container-features-test-lib
 check "eslint version is equal to 9.20.1" bash -c "eslint --version | grep 'v9.20.1'"
 check "nodemon is installed" nodemon --version
 check "prettier version is equal to 3.5.2" bash -c "prettier --version | grep '3.5.2'"
-check "@moonrepo/cli version is equal to 1.34.0" bash -c "moonrepo --version | grep 1.34.0"
+check "@moonrepo/cli version is equal to 1.34.0" bash -c "moon --version | grep 1.34.0"
 check "@biomejs/biome is installed" biome --version
 
 reportResults
