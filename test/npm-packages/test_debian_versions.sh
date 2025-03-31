652e968bd26a960f8beae1d3ea09628af731125a
George Cover <5285122+TheOptimist@users.noreply.github.com>
2025-03-31 13:32:00 -0400

    Update test/npm-packages/test_debian_versions.sh
    Co-authored-by: Arek Kalandyk <36413794+koralowiec@users.noreply.github.com>
    Signed-off-by: George Cover <5285122+TheOptimist@users.noreply.github.com>

--- test/npm-packages/test_debian_versions.sh
+++ test/npm-packages/test_debian_versions.sh
@@ -7,7 +7,7 @@ source dev-container-features-test-lib
 check "eslint version is equal to 9.20.1" bash -c "eslint --version | grep 'v9.20.1'"
 check "nodemon is installed" nodemon --version
 check "prettier version is equal to 3.5.2" bash -c "prettier --version | grep '3.5.2'"
-check "@moonrepo/cli version is equal to 1.34.0" bash -c "moon --version | grep '1.34.0'"
+check "@angular/cli version is equal to 19.2.1" bash -c "ng --version | grep '19.2.1'"
 check "@biomejs/biome is installed" biome --version
 
 reportResults
